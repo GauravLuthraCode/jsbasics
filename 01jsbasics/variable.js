d36fa15fb8c107c024e825479572a732e047c1dc
Gaurav <136916423+GauravLuthraCode@users.noreply.github.com>
2023-07-22 10:15:49 +0000

    Variable study

---
+++ 01jsbasics/variable.js
@@ -0,0 +1,17 @@
+const accountId = 123;
+
+let accountEmail = "gaurav@gmail.com";
+
+var accountPassword =  "12345";
+
+ accountCity = "Ambala";
+
+ //For the above line we can declare variable without using let var or const however, it is bad practice we should avoid it. 
+
+//accountId = 2; const cannot to declared twice
+
+console.table(typeof [accountId,accountEmail,accountPassword,accountCity]);
+
+
+
+
